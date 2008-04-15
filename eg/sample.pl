48d426279488aadb3a65d8369b1c8d5f9c275a6d
Jan Dubois <jand@activestate.com>
2008-04-15 22:07:35 +0000

    Add various sample scripts
    git-svn-id: https://dubiously.googlecode.com/svn/trunk@40 612f556c-7e46-0410-8898-af43e5f1171e

---
+++ eg/sample.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use Config qw(%Config);
+use PPM::Repositories qw(get list used_archs);
+
+for my $version (qw(5.6 5.8 5.10)) {
+    my $arch = "$Config{archname}";
+    $arch .= "-$version" unless $version eq "5.6";
+    print "Perl $arch\n";
+    for my $name (list($arch)) {
+	my %repo = get($name, $arch);
+	for my $url (qw(packlist packlist_noarch)) {
+	    next unless $repo{$url};
+	    printf("  %-12s %-30s %s\n", $name, $repo{desc}, $repo{$url});
+	}
+    }
+    print "\n";
+}
+
+print "\nused_arch\n";
+print "  $_\n" for used_archs();
+
+print "\nlog4perl\n";
+my %log4perl = get("log4perl");
+for (sort keys %log4perl) {
+    printf "  %-12s %s\n", $_, $log4perl{$_};
+}
